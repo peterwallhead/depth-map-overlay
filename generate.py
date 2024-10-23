280c35bd1b552b3e152d5331405bd762c9122b74
Peter Wallhead <peter.wallhead@gmail.com>
2024-10-23 22:38:31 +1100

    Calculate LiDAR FOV offset from camera

--- generate.py
+++ generate.py
@@ -24,15 +24,8 @@ LIDAR_DISTANCE_OFFSET_MM = -120
 # Approximates the number of pixels per mm
 VERTICAL_PX_PER_MM =  CAMERA_VERTICAL_CENTREPOINT_IN_FRAME_PX / CAMERA_VERTICAL_CENTREPOINT_IN_FRAME_MM
 
-# Approximates the width for each depth map marker
-HORIZONTAL_PX_PER_DEGREES_FOV = CAMERA_HORIZONTAL_RESOLUTION_PX / CAMERA_HORIZONTAL_FOV_DEGREES
-
 # Calculates the y position of the depth map markers
 MARKER_Y = CAMERA_VERTICAL_CENTREPOINT_IN_FRAME_PX - (LIDAR_VERTICAL_OFFSET_MM / VERTICAL_PX_PER_MM)
-
-# Constrain data used from the LiDAR to the camera's FOV
-MIN_HORIZONTAL_FOV = 360 - (CAMERA_HORIZONTAL_FOV_DEGREES / 2)
-MAX_HORIZONTAL_FOV = CAMERA_HORIZONTAL_FOV_DEGREES / 2
 
 VANISHING_POINT_Y = CAMERA_VERTICAL_RESOLUTION_PX - (CAMERA_VERTICAL_CENTREPOINT_IN_FRAME_MM * CAMERA_FOCAL_LENGTH_MM / VERTICAL_PX_PER_MM) - (LIDAR_VERTICAL_OFFSET_MM / VERTICAL_PX_PER_MM)
 
@@ -48,15 +41,43 @@ def depth_to_color(value):
 
     return (r, g, b)
 
+def calculate_initial_camera_fov_distance(data):
+    for angle, distance in data.items():
+        if float(angle) == 0.0:
+            return distance
+
+def calculate_lidar_fov(initial_fov_degrees, distance, distance_offset):
+    initial_fov_radians = math.radians(initial_fov_degrees)
+    
+    initial_width_half = distance * math.tan(initial_fov_radians / 2)
+    
+    new_distance = distance - distance_offset
+    
+    new_fov_half_radians = math.atan(initial_width_half / new_distance)
+    
+    new_fov_degrees = math.degrees(new_fov_half_radians * 2)
+
+    min_horizontal_lidar_fov = 360 - (new_fov_degrees / 2)
+    max_horizontal_lidar_fov = new_fov_degrees / 2
+    
+    return int(new_fov_degrees), int(min_horizontal_lidar_fov), int(max_horizontal_lidar_fov)
+
 def process_data_in_fov(data):
     data_in_fov = []
     maximum_distance_measurement = 0
 
+    # As the LiDAR is offset to the camera it has a different FOV to the camera
+    initial_camera_fov_distance = calculate_initial_camera_fov_distance(data)
+    horizontal_lidar_fov, min_horizontal_lidar_fov, max_horizontal_lidar_fov = calculate_lidar_fov(CAMERA_HORIZONTAL_FOV_DEGREES, initial_camera_fov_distance, LIDAR_DISTANCE_OFFSET_MM)
+    
+    # Approximates the width for each depth map marker
+    horizontal_px_per_degress_fov = CAMERA_HORIZONTAL_RESOLUTION_PX / horizontal_lidar_fov
+
     for angle, distance in data.items():
-        if (float(angle) >= MIN_HORIZONTAL_FOV or float(angle) <= MAX_HORIZONTAL_FOV) and distance != 0.0:
+        if (float(angle) >= min_horizontal_lidar_fov or float(angle) <= max_horizontal_lidar_fov) and distance != 0.0:
             distance = int((distance + LIDAR_DISTANCE_OFFSET_MM) * math.cos(math.radians(float(angle))))
         
-            if float(angle) >= MIN_HORIZONTAL_FOV:
+            if float(angle) >= min_horizontal_lidar_fov:
                 angle_key = -(360 - float(angle))
             else:
                 angle_key = float(angle)
@@ -65,12 +86,12 @@ def process_data_in_fov(data):
 
             if distance > maximum_distance_measurement:
                 maximum_distance_measurement = distance
-                vanishing_point_x = (CAMERA_HORIZONTAL_RESOLUTION_PX / 2) + (HORIZONTAL_PX_PER_DEGREES_FOV * angle_key)
+                vanishing_point_x = (CAMERA_HORIZONTAL_RESOLUTION_PX / 2) + (horizontal_px_per_degress_fov * angle_key)
                 vanishing_point_x_angle_key = angle_key
 
     data = sorted(data_in_fov, key=lambda x: x[0])
 
-    return data, maximum_distance_measurement, vanishing_point_x, vanishing_point_x_angle_key
+    return data, maximum_distance_measurement, vanishing_point_x, vanishing_point_x_angle_key, horizontal_px_per_degress_fov
 
 def calculate_slope_point_y_spacing(points, vanishing_point_angle):
     num_points_positive = 0
@@ -97,7 +118,7 @@ if __name__ == '__main__':
     with open(args.data, 'r', encoding="utf-8") as f:
         data = ast.literal_eval(f.read())
 
-    data_in_fov, maximum_distance_measurement, VANISHING_POINT_X, vanishing_point_x_angle_key = process_data_in_fov(data)
+    data_in_fov, maximum_distance_measurement, VANISHING_POINT_X, vanishing_point_x_angle_key, HORIZONTAL_PX_PER_DEGREES_FOV = process_data_in_fov(data)
 
     with Image.open(args.image) as im:
         grayscale_im = im.convert('L')
